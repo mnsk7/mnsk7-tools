313f380d81568e234892440e115e2c06aa786473
mnsk7 <WWW@mnsk7.pl>
2026-05-05 14:03:26 +0200

    Constrain PDP sticky gallery to buybox

--- wp-content/themes/mnsk7-storefront/woocommerce/content-single-product.php
+++ wp-content/themes/mnsk7-storefront/woocommerce/content-single-product.php
@@ -22,6 +22,7 @@ if ( post_password_required() ) {
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 
+	<div class="mnsk7-pdp-main">
 	<?php do_action( 'woocommerce_before_single_product_summary' ); ?>
 
 	<div class="summary entry-summary mnsk7-pdp-buybox">
@@ -54,6 +55,7 @@ if ( post_password_required() ) {
 		 */
 		do_action( 'woocommerce_single_product_summary' );
 		?>
+	</div>
 	</div>
 
 	<?php
